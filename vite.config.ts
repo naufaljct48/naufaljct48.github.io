72938ce6654723207481007dfc68a4fec6696a4c
Naufal <naufaljct48@gmail.com>
2024-12-21 13:36:27 +0700

    edit vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/naufaljct48.github.io/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
